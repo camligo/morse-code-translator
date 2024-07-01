c406cb1b9e8344ac07e38410c563506141a3a17a
camligo <camilla.ligovic@gmail.com>
2024-07-01 21:56:32 +1000

    adds setup & morse code translator function

---
+++ script.js
@@ -0,0 +1,37 @@
+const morseCode = {
+  "A": ".-",
+  "B": "-...",
+  "C": "-.-.",
+  "D": "-..",
+  "E": ".",
+  "F": "..-.",
+  "G": "--.",
+  "H": "....",
+  "I": "..",
+  "J": ".---",
+  "K": "-.-",
+  "L": ".-..",
+  "M": "--",
+  "N": "-.",
+  "O": "---",
+  "P": ".--.",
+  "Q": "--.-",
+  "R": ".-.",
+  "S": "...",
+  "T": "-",
+  "U": "..-",
+  "V": "...-",
+  "W": ".--",
+  "X": "-..-",
+  "Y": "-.--",
+  "Z": "--.."
+}
+
+const engToMorse = (str) => {
+  return str.toUpperCase().split("").map((char) => {
+  if (char === " ") {
+    return " / "; // handles spaces between words
+  }
+  return morseCode[char] || "";
+  }).join(" ");
+};
